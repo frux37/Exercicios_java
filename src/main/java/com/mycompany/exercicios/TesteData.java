967dd9d3a0a89224e7203ee5c44e24aadcffae8a
20212pf.cc0012 <20212pf.cc0012@pc-106310.passofundo.ifsul.local>
2024-03-04 10:59:52 -0300

    commit com arquivo TesteData

---
+++ src/main/java/com/mycompany/exercicios/TesteData.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.exercicios;
+import java.util.Date;
+
+/**
+ *
+ * @author 20212pf.cc0012
+ */
+public class TesteData {
+
+    public static void main(String[] args) {
+        Date hoje = new Date();
+        System.out.println(hoje);
+        
+    }
+    
+}
