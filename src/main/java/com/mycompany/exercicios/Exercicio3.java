e96a050da95b662aa0c1ec794a8820ffb0f5b3c5
20212pf.cc0012 <20212pf.cc0012@pc-106310.passofundo.ifsul.local>
2024-03-04 10:02:10 -0300

    simplesmente

--- src/main/java/com/mycompany/exercicios/Exercicio3.java
+++ src/main/java/com/mycompany/exercicios/Exercicio3.java
@@ -34,6 +34,7 @@ public class Exercicio3 {
         System.out.println("Código versionado hahaha");
         System.out.println("Mais um commit hahahaha");
         System.out.println("Alteração no código do exercicio 3");
+        System.out.print("olá mundo");
     }
     
 }
