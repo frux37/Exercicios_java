63ce0c5735e1922c6eade0bea97db54dd99248c7
20212pf.cc0012 <20212pf.cc0012@pc-106310.passofundo.ifsul.local>
2024-03-04 10:39:43 -0300

    commitado com alguns exemplos da aula

---
+++ src/main/java/com/mycompany/exercicios/TiposPrimitivos.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.exercicios;
+
+/**
+ *
+ * @author 20212pf.cc0012
+ */
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        //apenas declara a variável
+        int numeroInteiro;
+        //inicializa a variável que foi apenas declarada
+        numeroInteiro = 5;
+        //declara e inicializa;
+        double numeroDecimal = 2.3123;
+        //declarando e inicializand um char
+        char letra = 'a';
+        byte a = 123;
+        short b = 12331;
+        long c = 12938971298371293L;
+        float d = 123.23f;
+        boolean e = true;
+        //define mais de uma variável ao mesmo tempo
+        int x, y, z;
+        //definindo e inicializando mais de uma variável
+        int q = 55, w = 6, t = 44;
+        System.out.println(numeroDecimal);
+        System.out.println(numeroInteiro);
+        System.out.println(letra);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+    
+}
