caed9aa576e516958f0fc46da505754b07b373cf
20212pf.cc0012 <20212pf.cc0012@pc-225998.passofundo.ifsul.local>
2024-03-11 10:01:38 -0300

    teste commit

--- src/main/java/com/mycompany/exercicios/TesteData.java
+++ src/main/java/com/mycompany/exercicios/TesteData.java
@@ -14,6 +14,7 @@ public class TesteData {
     public static void main(String[] args) {
         Date hoje = new Date();
         System.out.println(hoje);
+        System.out.println(hoje);
         
     }    
 }
